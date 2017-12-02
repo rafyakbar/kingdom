682bf509e10e5ff1da9be0d3584ebe2ff9f73897
Rafy Aulia Akbar <rafy683@gmail.com>
2017-12-03 04:47:12 +0700

    penambahan bridge

---
+++ object/Bridge.h
@@ -0,0 +1,15 @@
+//
+// Created by Rafy on 02/12/2017.
+//
+
+#ifndef KINGDOM_BRIDGE_H
+#define KINGDOM_BRIDGE_H
+
+
+class Bridge {
+public:
+    static void model_0();
+};
+
+
+#endif //KINGDOM_BRIDGE_H
